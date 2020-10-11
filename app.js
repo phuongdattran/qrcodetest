460529fe4dabfbb3809a942574f1356845970229
phuongdattran <tran.phuongdat@gmail.com>
2020-10-11 18:35:40 +0200

    generator working

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
 const express = require("express");
+//const jsQR = require("jsqr");
+const QRCode = require('qrcode');
+
 const app = express();
-const jsQR = require("jsqr");
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static("./public"));
 
 app.get("/", (req, res, next) => {
   res.render('scanner.ejs');
 });
+
+app.get("/generator", (req, res, next) => {
+  res.render('generator.ejs');
+});
+
+app.post("/qrcode", (req, res, next) => {
+  QRCode.toDataURL(req.body.url, function (err, url) {
+    res.render('qrcode.ejs', {url});
+  });
+    
+
+});
+
+///////////////////////////////////////////////////
+
 
 module.exports = app;
