5feac967e31dae0c09da82fe11432d1832736d66
phuongdattran <tran.phuongdat@gmail.com>
2020-10-11 15:32:41 +0200

    add js file

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const jsQR = require("jsqr");
 
+app.use(express.static("./public"));
+
 app.get("/", (req, res, next) => {
   res.render('scanner.ejs');
 });
